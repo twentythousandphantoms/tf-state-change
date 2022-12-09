68265391d6f710916242df38739865c6e5805c4a
Andrei.X.Belov.-ND <andrei.x.belov.-nd@disney.com>
2022-12-09 16:34:04 +0300

    Fix(JARVIS-1020): [main] update the main sequencing

--- main.py
+++ main.py
@@ -34,23 +34,35 @@ def replaceResourceInstancesAttributes(state_a,
                                        resource_filter,
                                        attribute_name,
                                        match_instances_by="index_key"):
+    """
+    Extracts attribute <attribute_name> from state_b
+    Applies this attribute to instances of state_a resource
+
+    :param state_a:
+    :param state_b:
+    :param resource_filter: (dict) The query on which the resource to select, e.g. {"name": "nlb_broker_listeners"}
+    :param attribute_name: (str) The attribute name to replace, e.g. "default_action"
+    :param match_instances_by: (str) The attribute for match instances from state_a and state_b
+    :return: Returns the updated state_a instances
+    :rtype: List[Dict]
+    """
     instances_a = state_a.getResourceInstances(query=resource_filter)
     instances_b = state_b.getResourceInstances(query=resource_filter)
-    instances_b_updated = []
+    instances_a_updated = []
 
     if len(instances_a) == len(instances_b):
-        for instance_a in instances_a:
+        for instance_b in instances_b:
 
-            attribute_value_a = state_a.getInstanceAttrValue(instance=instance_a,
+            attribute_value_b = state_b.getInstanceAttrValue(instance=instance_b,
                                                              attribute_key=attribute_name)
 
-            for instance_b in instances_b:
-                if instance_b["attributes"][match_instances_by] == instance_a["attributes"][match_instances_by]:
-                    instance_b_updated = state_b.updateInstanceAttr(instance=instance_b,
+            for instance_a in instances_a:
+                if instance_a["attributes"][match_instances_by] == instance_b["attributes"][match_instances_by]:
+                    instance_a_updated = state_a.updateInstanceAttr(instance=instance_a,
                                                                     attribute_key=attribute_name,
-                                                                    attribute_value=attribute_value_a)
-                    instances_b_updated.append(instance_b_updated)
-    return instances_b_updated
+                                                                    attribute_value=attribute_value_b)
+                    instances_a_updated.append(instance_a_updated)
+    return instances_a_updated
 
 
 def main() -> None:
@@ -66,35 +78,48 @@ def main() -> None:
     state_a = TerraformState(filename=states[0])
     state_b = TerraformState(filename=states[1])
 
-    a_broker_listeners = state_a.getByQuery({"name": "nlb_broker_listeners"})
-    a_service_listeners = state_a.getByQuery({"name": "nlb_service_listeners"})
-    a_broker_certificate = state_a.getByQuery({"name": "nlb_certificate"})
-    a_acm_certificate_validation = state_a.getByQuery(
-        query={"name": "nlb_certificate_validation", "type": "aws_acm_certificate_validation"})
-    a_route53_certificate_validation = state_a.getByQuery(
-        query={"name": "nlb_certificate_validation", "type": "aws_route53_record"})
+    # Get resources from state_a
+    a_nlb = state_a.getByQuery({"name": "nlb"})
     a_nlb_domain = state_a.getByQuery({"name": "nlb_domain"})
     a_nlb_service = state_a.getByQuery({"name": "nlb_service"})
-    a_nlb = state_a.getByQuery({"name": "nlb"})
+    a_broker_certificate = state_a.getByQuery({"name": "nlb_certificate"})
+    a_route53_certificate_validation = state_a.getByQuery(
+        query={"name": "nlb_certificate_validation", "type": "aws_route53_record"})
+    a_acm_certificate_validation = state_a.getByQuery(
+        query={"name": "nlb_certificate_validation", "type": "aws_acm_certificate_validation"})
 
-    state_b.deleteByQuery({"name": "nlb_broker_listeners"})
-    state_b.deleteByQuery({"name": "nlb_service_listeners"})
-    state_b.deleteByQuery({"name": "nlb_certificate"})
-    state_b.deleteByQuery(query={"name": "nlb_certificate_validation", "type": "aws_acm_certificate_validation"})
-    state_b.deleteByQuery(query={"name": "nlb_certificate_validation", "type": "aws_route53_record"})
-    state_b.deleteByQuery({"name": "nlb_domain"})
-    state_b.deleteByQuery({"name": "nlb_service"})
-    state_b.deleteByQuery({"name": "nlb"})
+    a_broker_listeners = state_a.getByQuery({"name": "nlb_broker_listeners"})
+    a_service_listeners = state_a.getByQuery({"name": "nlb_service_listeners"})
 
-    state_b.addResource(a_broker_listeners[0])
-    state_b.addResource(a_service_listeners[0])
-    state_b.addResource(a_broker_certificate[0])
-    state_b.addResource(a_acm_certificate_validation[0])
-    state_b.addResource(a_route53_certificate_validation[0])
-    state_b.addResource(a_nlb_domain[0])
-    state_b.addResource(a_nlb_service[0])
-    state_b.addResource(a_nlb[0])
+    # Update listeners with target groups of cluster b
+    a_broker_listeners[0]["instances"] = replaceResourceInstancesAttributes(state_a=state_a,
+                                                                            state_b=state_b,
+                                                                            resource_filter={
+                                                                                "name": "nlb_broker_listeners"},
+                                                                            attribute_name="default_action",
+                                                                            match_instances_by="port")
+    a_service_listeners[0]["instances"] = replaceResourceInstancesAttributes(state_a=state_a,
+                                                                             state_b=state_b,
+                                                                             resource_filter={
+                                                                                 "name": "nlb_broker_listeners"},
+                                                                             attribute_name="default_action",
+                                                                             match_instances_by="port")
+    # Update resources in state_b
+    state_b.updateByQuery(query={"name": "nlb"}, new_resource=a_nlb[0])
+    state_b.updateByQuery(query={"name": "nlb_domain"}, new_resource=a_nlb_domain[0])
+    state_b.updateByQuery(query={"name": "nlb_service"}, new_resource=a_nlb_service[0])
+    state_b.updateByQuery(query={"name": "nlb_certificate"}, new_resource=a_broker_certificate[0])
+    state_b.updateByQuery(query={"name": "nlb_certificate_validation", "type": "aws_route53_record"},
+                          new_resource=a_route53_certificate_validation[0])
+    state_b.updateByQuery(query={"name": "nlb_certificate_validation", "type": "aws_acm_certificate_validation"},
+                          new_resource=a_acm_certificate_validation[0])
 
+    state_b.updateByQuery(query={"name": "nlb_broker_listeners"},
+                          new_resource=a_broker_listeners[0])
+    state_b.updateByQuery(query={"name": "nlb_service_listeners"},
+                          new_resource=a_service_listeners[0])
+
+    # Apply the updated state_b
     state_b.save(dst="modified")
     if not dry_run:
         state_b.upload(source="modified")
@@ -108,7 +133,7 @@ if __name__ == '__main__':
 
     handler = logging.StreamHandler(sys.stdout)
     handler.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(funcName)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
